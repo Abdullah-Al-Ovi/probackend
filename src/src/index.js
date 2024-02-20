f106e571b2bbd4c6a2d541d4fd7d52847306bab4
Noob-Ovi <abdullah.ovi098@gmail.com>
2024-02-21 01:07:06 +0600

    Connect to mongodb atlas

--- src/src/index.js
+++ src/src/index.js
@@ -1,5 +1,7 @@
 import connectDB from "./db/dbConnect.js";
-require('dotenv').config({path:'./env'})
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+dotenv.config({path:'./env'})
 
 connectDB()
 
